223aecdb81d9d2db3543bf9d2a17e843b3dbe05d
Davison Almeida <davison.almeida@somosrevvo.com.br>
2023-10-01 00:50:39 -0300

    Upgrade version with license

--- version.php
+++ version.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Version - Vue Template.
+ * Version - Vue Local Plugin template.
  * @package    local_vue
  * @copyright  2023 Davison Ramos <davison.ajr@gmail.com>
  * @author     Davison Ramos
